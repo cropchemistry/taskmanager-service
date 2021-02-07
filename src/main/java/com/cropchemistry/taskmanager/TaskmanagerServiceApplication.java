00b2b9a697a69f65eeab291bb0408149d9410805
Nischith BM <nischithbm@gmail.com>
2021-02-07 17:06:46 +0530

    initial commit - starter project

---
+++ src/main/java/com/cropchemistry/taskmanager/TaskmanagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cropchemistry.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cropchemistry/taskmanager/TaskmanagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cropchemistry.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
